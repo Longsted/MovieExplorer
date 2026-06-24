14553f231252b01de3d8ea3e62f616f3422fb80a
Longsted <eaa24nlgl@students.eaaa.dk>
2026-06-24 12:44:04 +0200

    Add movie service

---
+++ MovieExplorer.Application/Services/MovieService.cs
@@ -0,0 +1,31 @@
+﻿using MovieExplorer.Domain.Entities;
+using MovieExplorer.Domain.Interfaces;
+
+namespace MovieExplorer.Application.Services;
+
+public class MovieService
+{
+    private readonly IMovieRepository _movieRepository;
+    
+    public MovieService(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    public async Task<List<Movie>> GetMoviesByGenreAsync(int genreId)
+    {
+        return await _movieRepository.GetMoviesByGenreAsync();
+    }
+
+    public async Task<Movie?> GetMovieByIdAync(int movieId)
+    {
+        return await _movieRepository.GetMovieByIdAsync(movieId);
+    }
+
+    public async Task<int> GetMovieCountByGenreAsync(int genreId)
+    {
+        return await _movieRepository.GetMovieCountByGenreAsync(genreId);
+    }
+    
+    
+}
