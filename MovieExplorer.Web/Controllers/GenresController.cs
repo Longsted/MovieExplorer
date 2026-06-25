d7e6fb935ce4610381fe22367e9cb56e4751d953
Longsted <eaa24nlgl@students.eaaa.dk>
2026-06-25 11:36:16 +0200

    Add genre browsing feature

--- MovieExplorer.Web/Controllers/GenresController.cs
+++ MovieExplorer.Web/Controllers/GenresController.cs
@@ -1,6 +1,22 @@
-﻿namespace MovieExplorer.Web.Controllers;
+﻿using MovieExplorer.Application.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class GenresController
+
+namespace MovieExplorer.Web.Controllers;
+
+public class GenresController : Controller
 {
+    private readonly GenreService _genreService;
+
+    public GenresController(GenreService genreService)
+    {
+        _genreService = genreService;
+    }
     
+    public async Task<IActionResult> Index()
+    {
+        
+        var genres = await _genreService.GetGenreAsync();
+        return View(genres);
+    }
 }
--- MovieExplorer.Web/Controllers/HomeController.cs
+++ MovieExplorer.Web/Controllers/HomeController.cs
@@ -1,23 +1,20 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MovieExplorer.Application.Services;
+using MovieExplorer.Domain.Entities;
 using MovieExplorer.Web.Models;
 
 namespace MovieExplorer.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly MovieService _movieService;
     
-    public HomeController(MovieService movieService)
-    {
-        _movieService = movieService;
-    }
+    
+    
     
     public async Task<IActionResult> Index()
     {
-        var movies = await _movieService.GetMoviesByGenreAsync(28);
-        return Json(movies);
+        return View();
     }
 
     public IActionResult Privacy()
