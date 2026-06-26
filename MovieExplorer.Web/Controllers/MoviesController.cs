cf2b8c75d4624ffebd534dac3c04f1f14d828add
Longsted <eaa24nlgl@students.eaaa.dk>
2026-06-26 07:58:55 +0200

    implemented movie list by genre

---
+++ MovieExplorer.Web/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieExplorer.Application.Services;
+
+namespace MovieExplorer.Web.Controllers;
+
+public class MoviesController : Controller
+{
+    private readonly MovieService _movieService;
+
+    public MoviesController(MovieService movieService)
+    {
+        _movieService = movieService;
+    }
+
+    public async Task<IActionResult> ByGenre(int genreId)
+    {
+        var movies = await _movieService.GetMoviesByGenreAsync(genreId);
+        
+        return  View(movies);
+    }
+}
